38de146b3ca464cf493a1c4ed6a4db924c2da923
Bigjames728 <bigjames728@gmail.com>
2021-08-14 13:20:22 -0700

    Added a couple spaces

--- src/component/AsteroidList.js
+++ src/component/AsteroidList.js
@@ -21,6 +21,7 @@ class AsteroidList extends Component {
                         isLoaded: true,
                         asteroids: result.asteroids
                     });
+                    
                 },
                 (error) => {
                     this.setState({
@@ -35,6 +36,7 @@ class AsteroidList extends Component {
     render() {
 
         const { asteroids, isLoaded, error } = this.state;
+        
 
         if (!isLoaded) {
             return <div>Loading...</div>;
