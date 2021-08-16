ea711ca50fe670b76ec6c37a3dd62cc8bf9c9ecd
Bigjames728 <bigjames728@gmail.com>
2021-08-16 15:00:28 -0700

    Imported Header and global CSS

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // CSS Styles
+import './global.css';
 import './App.css';
 
 // Import Components
+import Header from './component/Header';
 import AsteroidList from './component/AsteroidList';
 
 function App() {
   return (
 
     <Router>
+    <Header />
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/asteroids" />} />
         <Route exact path="/asteroids" component={AsteroidList} />
